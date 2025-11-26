4dd42e122da5abfdb48061521ae3ed8a46e6ccb2
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-11-26 15:38:08 +0100

    Update DemoHost.cs

--- DemoApp/DemoHost.cs
+++ DemoApp/DemoHost.cs
@@ -6,17 +6,20 @@ using Nefarius.Backblaze.NativeApi.WebApi.Models;
 
 namespace DemoApp;
 
-internal sealed class DemoHost(IBackblazeUploader uploader, IBackblazeBucketClient client, ILogger<DemoHost> logger) : BackgroundService
+internal sealed class DemoHost(IBackblazeUploader uploader, IBackblazeBucketClient client, ILogger<DemoHost> logger)
+    : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        B2ListFileVersionsResponse versions = await client.ListFileVersionsAsync(cancellationToken: stoppingToken);
+
         B2ListFileNamesResponse files = await client.ListFileNamesAsync(cancellationToken: stoppingToken);
 
         foreach (B2StorageFile file in files.Files)
         {
             logger.LogInformation("Got remote file: {File}", file.FileName);
         }
-        
+
         /*await uploader.UploadAsync(@"D:\Downloads\Everything-1.4.1.1028.x64-Setup.exe", "target/Everything.exe",
             stoppingToken);*/
     }
